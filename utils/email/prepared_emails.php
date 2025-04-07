e9b94bd1b355e48630c5bcb8083d642810ab4873
Moad Abdellaoui <72411012+moadabdou@users.noreply.github.com>
2025-04-07 02:14:40 +0100

    Update prepared_emails.php
    fix  an issue with default email

--- utils/email/prepared_emails.php
+++ utils/email/prepared_emails.php
@@ -13,7 +13,7 @@ class PreparedEmails{
     public function accountIsReadyEmail(string $email, string $password, string $name): string | true{
         $login_url = "http://127.0.0.1/e-service/public/login.php";
         $subject = "Welcome Aboard! Your Account is Ready";
-        $body = "Dear ".$name.",\n\nWe're excited to inform you that your account has been successfully set up.\n\nYou can now access your account using the credentials below:\n\nPassword: ".$password."\n\nTo log in, visit: ".$login_url."\n\nFor security reasons, we highly recommend changing your password upon first login.\n\nIf you have any questions, feel free to reach out.\n\nBest regards,\nENSAH \nSupport Team\n";
+        $body = "Dear ".$name.",<br><br>We're excited to inform you that your account has been successfully set up.<br><br>You can now access your account using the credentials below<br><br>Password: ".$password."<br><br>To log in, visit: ".$login_url."<br><br>For security reasons, we highly recommend changing your password upon first login.<br><br>If you have any questions, feel free to reach out.<br><br>Best regards,<br>ENSAH <br>Support Team<br>";
         return $this->email_handler->sendTo($email, $subject, $body);
     }
 
