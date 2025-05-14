1e57d0665d2511a6ae97c342442f8a8e9ea40825
hassan-elkhatoury <hassanvivo@gmail.com>
2025-05-14 17:59:30 +0100

    UPDATE FORM

--- views/layouts/user_register_forms/vacataire_form.view.php
+++ views/layouts/user_register_forms/vacataire_form.view.php
@@ -1,14 +1,16 @@
 <div class="mb-4 col-lg-6 col-xxl-6 col-md-6 col-sm-12 col-12">
-    <label for="id_speciality" class="form-label">speciality</label>
+    <label for="id_speciality" class="form-label">Speciality</label>
     <select class="form-select <?= isset($errors["id_speciality"]) ? "is-invalid" : "" ?>" name="id_speciality" id="id_speciality">
-    <?php foreach($specialities as $speciality){?>
-        <option value="<?=$speciality["id_speciality"]?>"><?=$speciality["title"]?></option>
-    <?php }?>
+        <?php foreach ($specialities as $speciality) { ?>
+            <option value="<?= $speciality["id_speciality"] ?>"
+                <?= isset($_POST['id_speciality']) && $_POST['id_speciality'] == $speciality["id_speciality"] ? "selected" : "" ?>>
+                <?= $speciality["title"] ?>
+            </option>
+        <?php } ?>
     </select>
-    <?php if (isset($errors["id_speciality"])) {?>
+    <?php if (isset($errors["id_speciality"])) { ?>
         <div class="invalid-feedback">
             <?= $errors["id_speciality"] ?>
         </div>
-    <?php }?>
-</div>
+    <?php } ?>
 </div>
