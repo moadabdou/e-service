274a1e8a2ef4096d44255303cea18db41f05e0af
a-bkr <ayoubboukakar123@gmail.com>
2025-06-07 23:19:24 +0100

    merging 'origin/ayoube' into ayoube

--- views/pages/dashboard/dashboard.php
+++ views/pages/dashboard/dashboard.php
@@ -1,23 +1,33 @@
-<?php 
-require_once __DIR__."../../../layouts/sidebar/sidebar.php";
+<?php
+require_once __DIR__ . "../../../layouts/sidebar/sidebar.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/e-service/models/content/notification.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/e-service/models/entity/user.php";
 
 
-class DashBoard{
+class DashBoard
+{
 
-    public function view(string $role, string $active, string $content){
-        $user=(new UserModel())->getUser($_SESSION['id_user']);
+    public function view(string $role, string $active, string $content)
+    {
+        $user = (new UserModel())->getUser($_SESSION['id_user']);
         $Role = getRole($role);
         $sidebar = new SideBar($role);
         $sidebar_view = $sidebar->view($active);
         $notificatioModel = new NotificationModel();
         $unread_notifications_count = $notificatioModel->getUnreadNotificationCountByUserId($_SESSION["id_user"]);
-        require __DIR__."/dashboard.view.php";
+        require __DIR__ . "/dashboard.view.php";
     }
-
 }
+function getRole(string $role): string
+{
+    $roles = [
+        "admin" => "Administrateur",
+        "professor" => "Professeur",
+        "professor/chef_deparetement" => "Chef de Département",
+        "professor/coordonnateur" => "Coordonnateur",
+        "vacataire" => "Vacataire",
+        "general" => "Général"
+    ];
 
-
-
-?>
+    return $roles[$role] ?? "Utilisateur";
+}
