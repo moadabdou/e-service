679af52b113711e4720478109f6474ce0fbf8bf7
a-bkr <ayoubboukakar123@gmail.com>
2025-06-08 21:41:34 +0100

    fixing dashboard view

--- views/pages/dashboard/dashboard.php
+++ views/pages/dashboard/dashboard.php
@@ -4,28 +4,17 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/e-service/models/content/notification
 require_once $_SERVER['DOCUMENT_ROOT'] . "/e-service/models/entity/user.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/e-service/controllers/entity/user.php";
 
-class DashBoard{
+class DashBoard
+{
 
-    public function view(string $active, string $content){
-        $user=(new UserModel())->getUser($_SESSION['id_user']);
+    public function view(string $active, string $content)
+    {
+        $user = (new UserModel())->getUser($_SESSION['id_user']);
         $Role = (new UserController())->resolveRole($_SESSION['role']); //role to french 
-        $sidebar = new SideBar($_SESSION['role']); 
+        $sidebar = new SideBar($_SESSION['role']);
         $sidebar_view = $sidebar->view($active);
         $notificatioModel = new NotificationModel();
         $unread_notifications_count = $notificatioModel->getUnreadNotificationCountByUserId($_SESSION["id_user"]);
         require __DIR__ . "/dashboard.view.php";
     }
-}
-function getRole(string $role): string
-{
-    $roles = [
-        "admin" => "Administrateur",
-        "professor" => "Professeur",
-        "professor/chef_deparetement" => "Chef de Département",
-        "professor/coordonnateur" => "Coordonnateur",
-        "vacataire" => "Vacataire",
-        "general" => "Général"
-    ];
-
-    return $roles[$role] ?? "Utilisateur";
 }
